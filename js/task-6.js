4d184198565be72b00dbbf22d0cff608b0dbce9d
Roman Zhyhailo <r.zhigaylo@ukr.net>
2024-06-11 17:54:43 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,5 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
 const createBtn = document.querySelector('[data-create]');
@@ -20,15 +18,18 @@ function createBoxes() {
   }
   destroyBoxes();
   
+  const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     box.style.width = `${30 + i * 10}px`;
     box.style.height = `${30 + i * 10}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxContainer.append(box);
+    fragment.appendChild(box);
   }
-  
+
+  boxContainer.appendChild(fragment);
   input.value = '';
 }
 
